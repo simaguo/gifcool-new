79e15236afda4ae4bcb453f52a74bfd70b6c27e8
simaguo <2938475318@qq.com>
2018-04-19 12:35:03 +0800

    cors change

--- background/app/Http/Middleware/EnableCorsMiddleware.php
+++ background/app/Http/Middleware/EnableCorsMiddleware.php
@@ -10,22 +10,29 @@ class EnableCorsMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if($request->getMethod() == "OPTIONS") {
+        if ($request->getMethod() == "OPTIONS") {
             $response = new Response('', 200);
-        }else{
+        } else {
             $response = $next($request);
         }
 
-        $response->header('Access-Control-Allow-Origin','http://localhost:8080');
-        $response->header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Headers','Content-Type, X-Auth-Token, Origin, Authorization');
-        $response->header('Access-Control-Max-Age','1728000');
+        $allowed_domains = ['http://localhost:8080', ' https://www.gifcool.cn', 'https://gifcool.cn'];
+        $http_origin = $_SERVER['HTTP_ORIGIN'];
+        
+        if (in_array($http_origin, $allowed_domains)) {
+            $response->header('Access-Control-Allow-Origin', $http_origin);
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+            $response->header('Access-Control-Max-Age', '1728000');
+        }
+
+
         return $response;
     }
 }
